ae53321c6518e59edfb6a2d0682084d764c39113
Noel <noelshaju2003@gmail.com>
2025-04-11 15:26:48 +1000

    Finished FriendsFavoriteMovies Tutorial

--- FriendsFavoriteMovies/FriendsFavoriteMovies/MovieList.swift
+++ FriendsFavoriteMovies/FriendsFavoriteMovies/MovieList.swift
@@ -6,13 +6,33 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct MovieList: View {
+    @Query(sort: \Movie.title) private var movies: [Movie]
+    @Environment(\.modelContext) private var context
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationSplitView {
+            List {
+                ForEach(movies) { movie in
+                    NavigationLink(movie.title) {
+                        Text("Detail view for \(movie.title)")
+                            .navigationTitle("Movie")
+                            .navigationBarTitleDisplayMode(.inline)
+                    }
+                }
+            }
+            .navigationTitle("Movies")
+        } detail: {
+            Text("Select a movie")
+                .navigationTitle("Movie")
+                .navigationBarTitleDisplayMode(.inline)
+        }
     }
 }
 
 #Preview {
     MovieList()
+        .modelContainer(SampleData.shared.modelContainer)
 }
